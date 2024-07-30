49045724e42c89dacf8b48f35573b39bc9db3ff3
lo-franky <lo.frankyl@gmail.com>
2024-07-30 15:26:38 -0500

    finished react components lab

---
+++ src/components/WeatherForecast/WeatherForecast.jsx
@@ -0,0 +1,22 @@
+import './WeatherForecast.css'
+
+const WeatherForecast = (forecast) => {
+    const { day, img, imgAlt, conditions, time } = forecast;
+
+    return (
+        <div className="weather">
+            <h2>{day}</h2>
+            <img src={img} alt={imgAlt} />
+            <p>
+            <span>conditions: </span>
+            {conditions}
+            </p>
+            <p>
+            <span>time: </span>
+            {time}
+            </p>
+        </div>
+    );
+};
+  
+export default WeatherForecast;
